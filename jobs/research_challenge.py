1556b3a8e02e92daa9980f7205cfc5abac9d74de
Dušan Jovanović <dusandjovanovic@outlook.com>
2021-04-12 17:31:22 +0200

    Cleanup, Sentiment analysis added

--- jobs/research_challenge.py
+++ jobs/research_challenge.py
@@ -1,114 +1,272 @@
-import numpy as np
-import pandas as pd
-import json
-import glob
-import sys
-import matplotlib.pyplot as plt
-import seaborn as sns
-from collections import OrderedDict
 import os
+import sys
+from pyspark.sql import Window
+from pyspark.sql.types import StringType, ArrayType, StructField, StructType, IntegerType, MapType, DoubleType
+from pyspark.sql.functions import udf
+import pyspark.sql.functions as func
+import plotly.express as px
+from wordcloud import WordCloud, STOPWORDS
+from textblob import TextBlob
 import re
 
-meta = pd.read_csv("../data/open-research/metadata.csv")
-
-meta = meta[((meta['pdf_json_files'] == True) | (meta['pmc_json_files'] == True))]
-meta_sm = meta[['cord_uid', 'sha', 'pmcid', 'title', 'abstract', 'publish_time', 'url']]
-meta_sm.drop_duplicates(subset="title", keep=False, inplace=True)
-meta_sm.loc[meta_sm.publish_time == '2020-12-31'] = "2020-03-31"
-meta_sm.head()
-
-sys.path.insert(0, "../")
-
-root_path = '../data/open-research/'
-df = {"paper_id": [], "text_body": []}
-df = pd.DataFrame.from_dict(df)
-
-collect_json = glob.glob(f'{root_path}/**/*.json', recursive=True)
-
-for i, file_name in enumerate(collect_json):
-    row = {"paper_id": None, "text_body": None}
-    if i % 2000 == 0:
-        print("====processed " + str(i) + ' json files=====')
-        print()
-
-    with open(file_name) as json_data:
-
-        data = json.load(json_data, object_pairs_hook=OrderedDict)
-
-        row['paper_id'] = data['paper_id']
-
-        body_list = []
-
-        for _ in range(len(data['body_text'])):
-            try:
-                body_list.append(data['body_text'][_]['text'])
-            except:
-                pass
-
-        body = "\n ".join(body_list)
-
-        row['text_body'] = body
-        df = df.append(row, ignore_index=True)
-
-merge1 = pd.merge(meta_sm, df, left_on='sha', right_on=['paper_id'])
-merge2 = pd.merge(meta_sm, df, left_on='pmcid', right_on=['paper_id'])
-merge2.head()
-
-merge_final = merge2.append(merge1, ignore_index=True)
-merge_final.drop_duplicates(subset="title", keep=False, inplace=True)
-
-merge_final = merge2.append(merge1, ignore_index=True)
-merge_final.drop_duplicates(subset="title", keep=False, inplace=True)
-
-corona = merge_final[(merge_final['publish_time'] > '2019-11-01') & (
-    merge_final['text_body'].str.contains('nCoV|Cov|COVID|covid|SARS-CoV-2|sars-cov-2'))]
-corona.shape
-
-def clean_dataset(text):
-    text = re.sub('[\[].*?[\]]', '', str(text))  # remove in-text citation
-    text = re.sub(r'^https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)  # remove hyperlink
-    text = re.sub(r'\\b[A-Z a-z 0-9._ - ]*[@](.*?)[.]{1,3} \\b', '', text)  # remove email
-    text = re.sub(r'^a1111111111 a1111111111 a1111111111 a1111111111 a1111111111.*[\r\n]*', ' ',
-                  text)  # have no idea what is a11111.. is, but I remove it now
-    text = re.sub(r'  +', ' ', text)  # remove extra space
-    text = re.sub('[,\.!?]', '', text)
-    text = re.sub(r's/ ( *)/\1/g', '', text)
-    text = re.sub(r'[^\w\s]', '', text)  # strip punctions (recheck)
-    return text
+from dependencies.spark import start_spark
 
 
-corona['text_body'] = corona['text_body'].apply(clean_dataset)
-corona['title'] = corona['title'].apply(clean_dataset)
-corona['abstract'] = corona['abstract'].apply(clean_dataset)
-corona['text_body'] = corona['text_body'].map(lambda x: x.lower())
-coro = corona.reset_index(drop=True)
-coro.head()
+def main():
+    spark, sql_context, log, config = start_spark(
+        app_name='research_challenge',
+        files=['configs/research_challenge_config.json'])
 
-coro['count_abstract'] = coro['abstract'].str.split().map(len)
-coro['count_abstract'].sort_values(ascending=True)
+    log.warn('Running research_challenge analysis...')
 
-y = np.array(coro['count_abstract'])
-sns.distplot(y);
+    # extracting and transforming the dataset
+    data = extract_data(spark)
+    data_transformed = transform_data(data, sql_context)
 
-coro['count_text'] = coro['text_body'].str.split().map(len)
-coro['count_text'].sort_values(ascending=True)
+    # top paper authors
+    data_transformed = transform_papers_and_authors(data_transformed)
+    load_data(data_transformed, "paper_authors")
 
-import seaborn as sns
-import matplotlib.pyplot as plt
+    # extracting abstracts
+    data_transformed = transform_papers_and_abstracts(data_transformed)
+    load_data(data_transformed, "paper_abstracts")
 
-y = np.array(coro['count_abstract'])
+    # displaying common words in abstracts
+    data_transformed = transform_abstracts_words(data_transformed)
+    load_data(data_transformed, "paper_abstracts")
 
-sns.distplot(y);
+    log.warn('Terminating research_challenge analysis...')
 
-coro['count_text'] = coro['text_body'].str.split().map(len)
-coro['count_text'].sort_values(ascending=True)
+    spark.stop()
+    return None
 
-y = np.array(coro['count_text'])
-sns.distplot(y);
 
-coro2=coro[((coro['count_text']>500)&(coro['count_text']<4000))]
-coro2.to_csv("corona.csv",index=False)
-test=coro2[coro2['count_abstract']<5]
-train= coro2.drop(test.index)
-train=train.reset_index(drop=True)
-test=test.reset_index(drop=True)
+def extract_data(spark):
+    base = "../data/open-research"
+
+    sources = [
+        "document_parses",
+    ]
+
+    sub_sources = [
+        "pdf_json_partial",
+    ]
+
+    dataframe = None
+
+    for source in sources:
+        for sub_source in sub_sources:
+            path = f"{base}/{source}/{sub_source}"
+
+            if not os.path.exists(path):
+                print(f"Path {path} does not exist", file=sys.stderr)
+                continue
+
+            df = (
+                spark.read.json(path, schema=generate_cord19_schema(), multiLine=True).withColumn("source",
+                                                                                                  func.lit(source))
+            )
+            if not dataframe:
+                dataframe = df
+            else:
+                dataframe = dataframe.union(df)
+
+    return dataframe
+
+
+def transform_data(frame, sql_context):
+    dt_transformed = frame
+    dt_transformed = dt_transformed.fillna("NA")
+
+    return dt_transformed
+
+
+def transform_papers_and_authors(dataframe):
+    df_authors = dataframe.select("paper_id", func.explode("metadata.authors").alias("author")) \
+        .select("paper_id", "author.*")
+    df_authors.select("first", "middle", "last", "email").where("email <> ''").show(n=10)
+
+    return dataframe
+
+
+def transform_papers_and_abstracts(dataframe):
+    df_abstracts = (
+        dataframe.select("paper_id", func.posexplode("abstract").alias("pos", "value"))
+            .select("paper_id", "pos", "value.text")
+            .withColumn("ordered_text", func.collect_list("text").over(Window.partitionBy("paper_id").orderBy("pos")))
+            .groupBy("paper_id")
+            .agg(func.max("ordered_text").alias("sentences"))
+            .select("paper_id", func.array_join("sentences", " ").alias("abstract"))
+            .withColumn("words", func.size(func.split("abstract", "\s+")))
+    )
+
+    df_abstracts.show(n=10)
+
+    return df_abstracts
+
+
+def transform_abstracts_words(dataframe):
+    udf_function_clean = udf(generate_cleaned_abstracts, StringType())
+    udf_function_sentiment = udf(generate_sentiment, DoubleType())
+    stopwords = set(STOPWORDS)
+
+    dataframe = dataframe.withColumn("clean_abstract", udf_function_clean("abstract"))
+    dataframe = dataframe.withColumn("sentiment_abstract", udf_function_sentiment("clean_abstract"))
+
+    dataframe_pd = dataframe.toPandas()
+    fig = px.histogram(dataframe_pd, x="words", y="sentiment_abstract", marginal="rug", hover_data=dataframe_pd.columns)
+    fig.show()
+
+    text = dataframe_pd["clean_abstract"].values
+    wordcloud = WordCloud(width=1000, height=500, stopwords=stopwords, background_color="white").generate(str(text))
+    fig = px.imshow(wordcloud)
+    fig.show()
+
+    return dataframe
+
+
+def load_data(dataframe, name):
+    (dataframe
+     .coalesce(1)
+     .write
+     .json(name, mode='overwrite'))
+    return None
+
+
+def generate_cleaned_abstracts(abstract):
+    abstract = re.sub('[^a-zA-Z]', ' ', abstract)
+    abstract = abstract.lower()
+    abstract = abstract.split()
+    abstract = ' '.join(abstract)
+
+    return abstract
+
+
+def generate_sentiment(sentence):
+    temp = TextBlob(sentence).sentiment[0]
+    if temp == 0.0:
+        return 0.0  # Neutral
+    elif temp >= 0.0:
+        return 1.0  # Positive
+    else:
+        return 2.0  # Negative
+
+
+def generate_cord19_schema():
+    author_fields = [
+        StructField("first", StringType()),
+        StructField("middle", ArrayType(StringType())),
+        StructField("last", StringType()),
+        StructField("suffix", StringType()),
+    ]
+
+    authors_schema = ArrayType(
+        StructType(
+            author_fields
+            + [
+                StructField(
+                    "affiliation",
+                    StructType(
+                        [
+                            StructField("laboratory", StringType()),
+                            StructField("institution", StringType()),
+                            StructField(
+                                "location",
+                                StructType(
+                                    [
+                                        StructField("addrLine", StringType()),
+                                        StructField("country", StringType()),
+                                        StructField("postBox", StringType()),
+                                        StructField("postCode", StringType()),
+                                        StructField("region", StringType()),
+                                        StructField("settlement", StringType()),
+                                    ]
+                                ),
+                            ),
+                        ]
+                    ),
+                ),
+                StructField("email", StringType()),
+            ]
+        )
+    )
+
+    spans_schema = ArrayType(
+        StructType(
+            [
+                StructField("start", IntegerType()),
+                StructField("end", IntegerType()),
+                StructField("text", StringType()),
+                StructField("ref_id", StringType()),
+            ]
+        )
+    )
+
+    section_schema = ArrayType(
+        StructType(
+            [
+                StructField("text", StringType()),
+                StructField("cite_spans", spans_schema),
+                StructField("ref_spans", spans_schema),
+                StructField("eq_spans", spans_schema),
+                StructField("section", StringType()),
+            ]
+        )
+    )
+
+    bib_schema = MapType(
+        StringType(),
+        StructType(
+            [
+                StructField("ref_id", StringType()),
+                StructField("title", StringType()),
+                StructField("authors", ArrayType(StructType(author_fields))),
+                StructField("year", IntegerType()),
+                StructField("venue", StringType()),
+                StructField("volume", StringType()),
+                StructField("issn", StringType()),
+                StructField("pages", StringType()),
+                StructField(
+                    "other_ids",
+                    StructType([StructField("DOI", ArrayType(StringType()))]),
+                ),
+            ]
+        ),
+        True,
+    )
+
+    ref_schema = MapType(
+        StringType(),
+        StructType(
+            [
+                StructField("text", StringType()),
+                StructField("latex", StringType()),
+                StructField("type", StringType()),
+            ]
+        ),
+    )
+
+    return StructType(
+        [
+            StructField("paper_id", StringType()),
+            StructField(
+                "metadata",
+                StructType(
+                    [
+                        StructField("title", StringType()),
+                        StructField("authors", authors_schema),
+                    ]
+                ),
+                True,
+            ),
+            StructField("abstract", section_schema),
+            StructField("body_text", section_schema),
+            StructField("bib_entries", bib_schema),
+            StructField("ref_entries", ref_schema),
+            StructField("back_matter", section_schema),
+        ]
+    )
+
+
+if __name__ == '__main__':
+    main()
